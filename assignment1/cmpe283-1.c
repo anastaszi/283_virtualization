ca28e7e8745993ce869ca657a3014f55e1665688
anastaszi <anastasba@gmail.com>
2020-10-25 18:06:15 -0700

    adding all control names

--- assignment1/cmpe283-1.c
+++ assignment1/cmpe283-1.c
@@ -51,6 +51,111 @@ struct capability_info pinbased[5] =
 };
 
 /*
+ * Processor-Based capabilities
+ * See SDM volume 3, section 24.6.2
+ */
+struct capability_info procbased[21] =
+{
+	{ 2, "Interrupt-window exiting" },
+	{ 3, "Use TSC offsetting" },
+	{ 7, "HLT exiting" },
+	{ 9, "INVLPG exiting" },
+	{ 10, "MWAIT exiting" },
+	{ 11, "RDPMC exiting" },
+	{ 12, "RDTSC exiting" },
+	{ 15, "CR3-load exiting" },
+	{ 16, "CR3-store exiting" },
+	{ 19, "CR8-load exiting" },
+	{ 20, "CR8-store exiting" },
+	{ 21, "Use TPR shadow" },
+	{ 22, "NMI-window exiting" },
+	{ 23, "MOV-DR exiting" },
+	{ 24, "Unconditional I/O exiting" },
+	{ 25, "Use I/O bitmaps" },
+	{ 27, "Monitor trap flag" },
+	{ 28, "Use MSR bitmaps" },
+	{ 29, "MONITOR exiting" },
+	{ 30, "PAUSE exiting" },
+	{31, "Activate secondary controls"}
+};
+/*
+ * Secondary Processor-Based capabilities
+ * See SDM volume 3, section 24.6.2
+ */
+struct capability_info sec_procbased[27] =
+{
+	{ 0, "Virtualize APIC accesses" },
+	{ 1, "Enable EPT" },
+	{ 2, "Descriptor-table exiting" },
+	{ 3, "Enable RDTSCP" },
+	{ 4, "Virtualize x2APIC mode" },
+	{ 5, "Enable VPID" },
+	{ 6, "WBINVD exiting" },
+	{ 7, "Unrestricted guest" },
+	{ 8, "APIC-register virtualization" },
+	{ 9, "Virtual-interrupt delivery" },
+	{ 10, "PAUSE-loop exiting" },
+	{ 11, "RDRAND exiting" },
+	{ 12, "Enable INVPCID" },
+	{ 13, "Enable VM functions" },
+	{ 14, "VMCS shadowing" },
+	{ 15, "Enable ENCLS exiting" },
+	{ 16, "RDSEED exiting" },
+	{ 17, "Enable PML" },
+	{ 18, "EPT-violation #VE" },
+	{ 19, "Conceal VMX from PT" },
+	{20, "Enable XSAVES/XRSTORS"},
+	{ 22, "Mode-based execute control for EPT" },
+	{ 23, "Sub-page write permissions for EPT" },
+	{ 24, "Intel PT uses guest physical addresses" },
+	{ 25, "Use TSC scaling" },
+	{ 26, "Enable user wait and pause" },
+	{28, "Enable ENCLV exiting"}
+};
+
+/*
+ * Exit Contol capabilities
+ * See SDM volume 3, section 24.7.1
+ */
+struct capability_info exit_controls[14] =
+{
+	{ 2, "Save debug controls" },
+	{ 9, "Host address-space size" },
+	{ 12, "Load IA32_PERF_GLOBAL_ CTRL" },
+	{ 15, "Acknowledge interrupt on exit" },
+	{ 18, "Save IA32_PAT" },
+	{ 19, "Load IA32_PAT" },
+	{ 20, "Save IA32_EFER" },
+	{ 21, "Load IA32_EFER" },
+	{ 22, "Save VMX- preemption timer value" },
+	{ 23, "Clear IA32_BNDCFGS" },
+	{ 24, "Conceal VMX from PT" },
+	{ 25, "Clear IA32_RTIT_CTL" },
+	{ 28, "Load CET state" },
+	{ 29, "Load PKRS" }
+};
+
+/*
+ * Entry Contol capabilities
+ * See SDM volume 3, section 24.8.1
+ */
+struct capability_info entry_controls[12] =
+{
+	{ 2, "Load debug controls" },
+	{ 9, "IA-32e mode guest" },
+	{ 10, "Entry to SMM" },
+	{ 11, "Deactivate dual- monitor treatment" },
+	{ 13, "Load IA32_PERF_GLOBA L_CTRL" },
+	{ 14, "Load IA32_PAT" },
+	{ 15, "Load IA32_EFER" },
+	{ 16, "Load IA32_BNDCFGS" },
+	{ 17, "Conceal VMX from PT" },
+	{ 18, "Load IA32_RTIT_CTL" },
+	{ 20, "Load CET state" },
+	{ 22, "Load PKRS" }
+};
+
+/*
  * report_capability
  *
  * Reports capabilities present in 'cap' using the corresponding MSR values
@@ -94,15 +199,20 @@ detect_vmx_features(void)
 {
 	int i = 0;
 	uint32_t lo, hi;
-	struct custom_msr_info array[1] = {
-		{IA32_VMX_PINBASED_CTLS, pinbased, 5, "Pinbased Controls MSR: "}
+	struct custom_msr_info array[NUMBER_OF_MSRS] = {
+		/* Pinbased controls */
+		{IA32_VMX_PINBASED_CTLS, pinbased, 5, "Pinbased Controls MSR: "},
+		{IA32_VMX_PROCBASED_CTLS, procbased, 21, "Processor-Based Controls MSR: "},
+		{IA32_VMX_PROCBASED_CTLS2, sec_procbased, 27, "Secondary Processor-Based MSR: "},
+		{IA32_VMX_EXIT_CTLS, exit_controls, 14, "Exit Controls MSR: "},
+		{IA32_VMX_ENTRY_CTLS, entry_controls, 12, "Entry Controls MSR: "}
 	};
 
 	/* Pinbased controls */
-	while (i < 1) {
+	while (i < NUMBER_OF_MSRS) {
 		rdmsr(array[i].address, lo, hi);
-		printk("%s", array[i].msg);
-		pr_info("0x%llx\n",
+		//printk("%s", array[i].msg);
+		pr_info("%s0x%llx\n",array[i].msg,
 			(uint64_t)(lo | (uint64_t)hi << 32));
 		report_capability(array[i].info, array[i].num_of_controls, lo, hi);
 		i++;
